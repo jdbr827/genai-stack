9029bda0e2660acae987572bc4a3527104e1b805
Jacob Reinhart <reinhartjacob827@gmail.com>
2025-02-04 14:30:34 -0500

    same as above

--- call_loader.py
+++ call_loader.py
@@ -1,14 +1,15 @@
 from flask import Flask, request, jsonify
 
-app = Flask(__name__)
 
-@app.route('/upload', methods=['GET'])
-def upload_file():
-    if request.is_json:
-        data = request.get_json()
-        return jsonify({"message": "JSON received", "data": data}), 200
-    else:
-        return jsonify({"message": "Request is not JSON"}), 400
+def create_app():
+    app = Flask(__name__)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+    @app.route('/upload', methods=['GET'])
+    def upload_file():
+        if request.is_json:
+            data = request.get_json()
+            return jsonify({"message": "JSON received", "data": data}), 200
+        else:
+            return jsonify({"message": "Request is not JSON"}), 400
+    
+    return app
