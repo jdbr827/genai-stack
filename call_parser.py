4fa5f10813ccada207844d0cfc64c2b37dd75f87
Jacob Reinhart <reinhartjacob827@gmail.com>
2025-02-05 16:12:55 -0500

    added transcripts

--- call_parser.py
+++ call_parser.py
@@ -1,3 +1,4 @@
+import json
 import os
 from utils import create_constraints, create_vector_index
 from langchain_community.graphs import Neo4jGraph
@@ -28,11 +29,13 @@ def parse_phone_call(phone_call, logger):
     my_number = phone_call.get('my_number')
     other_number = phone_call.get('other_party_number')
     call_id = phone_call.get('uuid')
+    phone_call['transcript'] = get_transcript(phone_call, logger)
     if my_number:
         import_query = f"""
         MERGE (me:PhoneNumber {{number: '{my_number}'}})
         MERGE (them:PhoneNumber {{number: '{other_number}'}})
         MERGE (callNum:uuid {{number: '{call_id}'}})
+        SET callNum.transcript = '{phone_call.get('transcript')}'
         MERGE (me)-[:SPOKE_TO]-(them)
         MERGE (me)-[:MADE_CALL]-(callNum)
         MERGE (them)-[:MADE_CALL]-(callNum)
@@ -41,4 +44,8 @@ def parse_phone_call(phone_call, logger):
         # Execute the query using your Neo4j driver here
     neo4j_graph.query(import_query, {"data":phone_call})
 
-
+def get_transcript(phone_call, logger):
+    # Parse each utterance in the phone call to get its text, joining them together with newlines
+    #logger.info("GETTING TRANSCRIPT")
+    transcript = "".join([utterance.get('text').replace("'", "\\'") + "\n" for utterance in phone_call.get('utterances')])
+    return str(transcript)
